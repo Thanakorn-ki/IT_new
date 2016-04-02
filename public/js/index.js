b44277c812050a26cac1995af4740ed1833be314
kingkong64 <kingkong2103@gmail.com>
2016-04-02 21:26:03 +0700

    Mockup

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
-/* global angular */
+/* global angular, $ */
 angular.module('itApp', [])
   .controller('itCtrl', function ($scope) {
-    $scope.test = 'GG'
+    $scope.test = ['ff',
+      '55',
+      '66'
+    ]
   })
+$(document).ready(function () {
+  $('.slider').slider({full_width: true})
+})
+
+$(document).ready(function () {
+  $('.collapsible').collapsible({
+    accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+  })
+})
+
+$(document).ready(function () {
+  $('.parallax').parallax()
+})
